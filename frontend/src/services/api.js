ff11a582044f6bbd1be1766957c41742b21cfc00
Sumitpargunde <115924571+Sumitpargunde@users.noreply.github.com>
2024-11-23 13:59:39 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:5000";
+const API_URL = "https://note-keeper-app-sumit-p.onrender.com";
 
 export const getNotes = async () => {
   const response = await axios.get(`${API_URL}/notes`);
